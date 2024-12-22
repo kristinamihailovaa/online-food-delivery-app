6ac17576f93f5b2065e4e824663676c8cbdc365e
Ati <ativassileva@gmail.com>
2024-12-22 11:16:23 +0200

    Renamed component folders

--- food-delivery-UI/food-delivery-ui/src/App.js
+++ food-delivery-UI/food-delivery-ui/src/App.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/HomePage/Home';
-import About from './components/AboutPage/About';
-import Contact from './components/ContactPage/Contact';
-import ScrollToTop from './components/Common/ScrollToTop';
-import Menu from './components/MenuPage/Menu';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import ScrollToTop from './components/common/ScrollToTop';
+import Menu from './components/menu/Menu';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
   return (
